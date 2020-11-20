9ea6f171676c661cb577f947b6ca0233492913fe
Paul Whiting <paul.whiting@stage2sec.com>
2020-11-20 11:19:28 -0700

    Make 'client' a soft exclude.

--- mage/api_resources/abstract/util.py
+++ mage/api_resources/abstract/util.py
@@ -41,8 +41,8 @@ def update_subfields(field, key, value, query):
     """
     This function goes through a request to the server to select and exclude fields from the request.
     """
-    soft_exclude=['asm_graph', 'recon']
-    hard_exclude = ['client', 'arn', 'execution_arn', 'write_key', 'secret_key', 'session_token', 'additional_data', 'stripe_subscription_id', 'stripe_latest_invoice_status', 'stripe_customer_id', 'stripe_customer', 'stripe_subscription_status']
+    soft_exclude=['asm_graph', 'recon', 'client']
+    hard_exclude = ['arn', 'execution_arn', 'write_key', 'secret_key', 'session_token', 'additional_data', 'stripe_subscription_id', 'stripe_latest_invoice_status', 'stripe_customer_id', 'stripe_customer', 'stripe_subscription_status']
 
     from ...schema import schema
     all_fields = [schema.Schedule]
